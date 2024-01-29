44d2e382722c8b40dae831c3150ec75da16456d0
ismaelargence <ismaelargence3@gmail.com>
2024-01-29 17:32:22 +0100

    pour matheo

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./home/isbuntu/ismaelargence.github.io/index.html", "./home/isbuntu/ismaelargence.github.io/src/**/*.{html,js}"],
+  content: ["./index.html", "./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
